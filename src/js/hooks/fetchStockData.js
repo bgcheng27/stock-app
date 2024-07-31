ad3f646dbe99acae530606a2530db7570a6bae9b
Brian Cheng <bgcheng27@gmail.com>
2024-07-31 14:53:21 -0700

    Demo version of the project; removed search bar

--- src/js/hooks/fetchStockData.js
+++ src/js/hooks/fetchStockData.js
@@ -1,74 +1,40 @@
 import { financialsDemo, getFinancials } from "../../api/financials";
-import { getQuote, getQuoteDemo, getTimeSeries, getTimeSeriesDemo, getOverview, getOverviewDemo } from "../../api/marketData";
-import { ERROR_MESSAGES } from "../mockData"
-import { INCOME_STATEMENT, BALANCE_SHEET, CASH_FLOW } from "../data/financialsConfig"
+import {
+  getQuote,
+  getQuoteDemo,
+  getTimeSeries,
+  getTimeSeriesDemo,
+  getOverview,
+  getOverviewDemo,
+} from "../../api/marketData";
+import { ERROR_MESSAGES } from "../mockData";
+import {
+  INCOME_STATEMENT,
+  BALANCE_SHEET,
+  CASH_FLOW,
+} from "../data/financialsConfig";
 
 // maybe a custom hook
 export async function fetchStockData(isDemo, signal, symbol) {
-  let incomeStatementData,
-    balanceSheetData,
-    cashFlowData,
-    marketData,
-    quoteData,
-    description,
-    expectedSymbol;
-
   const GRAPH_INTERVAL = isDemo ? "5min" : "1min";
-  
-  if (isDemo) {
-    incomeStatementData = financialsDemo(INCOME_STATEMENT.apiCall, {
-      signal,
-    });
-    balanceSheetData = financialsDemo(BALANCE_SHEET.apiCall, { signal });
-    cashFlowData = financialsDemo(CASH_FLOW.apiCall, { signal });
-    marketData = await getTimeSeriesDemo({ signal });
-    quoteData = await getQuoteDemo({ signal });
-    description = await getOverviewDemo({ signal })
 
-    console.log(description)
-  } else {
-    incomeStatementData = getFinancials(
-      INCOME_STATEMENT.apiCall,
-      symbol.toUpperCase(),
-      { signal }
-    );
-    balanceSheetData = getFinancials(
-      BALANCE_SHEET.apiCall,
-      symbol.toUpperCase(),
-      { signal }
-    );
-    cashFlowData = await getFinancials(
-      CASH_FLOW.apiCall,
-      symbol.toUpperCase(),
-      { signal }
-    );
-    marketData = await getTimeSeries(
-      "TIME_SERIES_INTRADAY",
-      symbol.toUpperCase(),
-      GRAPH_INTERVAL,
-      "false",
-      "full",
-      { signal }
-    );
-    quoteData = await getQuote(symbol.toUpperCase(), { signal });
-    description = await getOverview(symbol.toUpperCase(), { signal })
-  }
-
-  if (marketData["Meta Data"]) {
-    expectedSymbol = marketData["Meta Data"]["2. Symbol"]
-  } else {
-    expectedSymbol = undefined
-  }
-
+  let incomeStatementData = financialsDemo(INCOME_STATEMENT.apiCall, {
+    signal,
+  });
+  let balanceSheetData = financialsDemo(BALANCE_SHEET.apiCall, { signal });
+  let cashFlowData = financialsDemo(CASH_FLOW.apiCall, { signal });
+  let marketData = await getTimeSeriesDemo({ signal });
+  let quoteData = await getQuoteDemo({ signal });
+  let description = await getOverviewDemo({ signal });
 
   return {
     incomeStatementData: await incomeStatementData,
     balanceSheetData: await balanceSheetData,
     cashFlowData: await cashFlowData,
 
-    symbol: expectedSymbol,
+    symbol,
     dataPoints: marketData[`Time Series (${GRAPH_INTERVAL})`],
     quoteData: quoteData["Global Quote"],
-    description: description["Description"]
+    description: description["Description"],
   };
 }
--- src/layouts/RouteLayout.jsx
+++ src/layouts/RouteLayout.jsx
@@ -1,6 +1,5 @@
 import { Outlet, useNavigation } from "react-router-dom";
 import { ContentErrorBoundary } from "./error-boundaries/ContentErrorBoundary";
-import { TopBar } from "./TopBar";
 
 
 export function RouteLayout() {
@@ -11,12 +10,8 @@ export function RouteLayout() {
     <>
     <div className={isLoading ? "loading-spinner" : undefined}></div>
       <div id="wrapper" className={isLoading ? "loading" : undefined}>
-        {/* Sidebar */}
-        {/* <SideBar /> */}
-
         <div id="content-wrapper" className="d-flex flex-column">
           <div id="content">
-            <TopBar />
             <div className="container-fluid py-2 px-5">
               <ContentErrorBoundary>
                 <Outlet />
